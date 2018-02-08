64d2afff32750e8008a232b409303b8c342144f9
Erik <e.vandervegt03@gmail.com>
2018-02-08 20:30:32 +0100

    database

---
+++ General.php
@@ -0,0 +1,18 @@
+<?php
+
+function connectionDB() {
+
+    $hostname = 'localhost';
+    $databasenaam = 'reviewdehersteltrainer';
+    $username = 'root';
+    $password = '';
+
+    $conn = new mysqli($hostname, $username, $password, $databasenaam);
+    return $conn;
+}
+
+
+function get_post($conn, $var) {
+    return $conn->real_escape_string($_POST[$var]);
+}
+?> 
