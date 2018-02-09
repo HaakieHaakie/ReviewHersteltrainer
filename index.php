85b87f3b35d15fe6455983b1451ca6a83ee8e36e
Erik <e.vandervegt03@gmail.com>
2018-02-09 18:47:46 +0100

    reviews lezen

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $conn = connectionDB();
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
+
             .error {color: #FF0000;}
+            #pre {
+                color: #4a4b63;
+            }
 
             .field_set{
                 border-color:#DA3701;
@@ -51,12 +55,19 @@ $conn = connectionDB();
             }
 
             .grid-container > .item4 {
-                background-color: lightgrey;
+                background-color: #ececec;
                 border-radius: 25px;
                 font-family: 'Open Sans';
                 font-weight: 800;
                 font-size: 14px;
                 color: #DA3701;
+            }
+
+            .grid-container > .item2 {
+                width: 200px;
+            }
+            .grid-container > .item5 {
+                width: 200px;
             }
         </style>
     </head>
@@ -152,36 +163,39 @@ $conn = connectionDB();
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                     <fieldset class="field_set">
                         <legend><p><span class="error">* verplicht veld.</span></p></legend>
-                        Voornaam: <input type="text" name="naam" value="<?php echo $naam; ?>">
-                        <span class="error">* <?php echo $naamErr; ?></span>
-                        <br><br>
-                        Achternaam: <input type="text" name="achternaam" value="<?php echo $achternaam; ?>">
-                        <span class="error">* <?php echo $achternaamErr; ?></span>
-                        <br><br>
-                        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
-                        <span class="error">* <?php echo $emailErr; ?></span>
-                        <br><br>
-                        Review: <textarea name="review" rows="5" cols="40"><?php echo $review; ?></textarea>
-                        <br><br>
-                        Beoordeling:
-                        <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "uitstekend") echo "checked"; ?> value="uitstekend">Uitstekend
-                        <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "goed") echo "checked"; ?> value="goed">Goed
-                        <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "matig") echo "checked"; ?> value="matig">Matig
-                        <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "slecht") echo "checked"; ?> value="slecht">Slecht
-                        <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "nietAanbevelen") echo "checked"; ?> value="nietAanbevelen">Niet aan te bevelen
+                        <div id="input">
+                            <b>Voornaam:</b><br> <input type="text" name="naam" value="<?php echo $naam; ?>">
+                            <span class="error">* <?php echo $naamErr; ?></span>
+                            <br><br>
+                            <b>Achternaam:</b><br> <input type="text" name="achternaam" value="<?php echo $achternaam; ?>">
+                            <span class="error">* <?php echo $achternaamErr; ?></span>
+                            <br><br>
+                            <b>E-mail:</b><br> <input type="text" name="email" value="<?php echo $email; ?>">
+                            <span class="error">* <?php echo $emailErr; ?></span>
+                            <br><br>
+                            <b>Review:</b><br> <textarea name="review" rows="5" cols="40"><?php echo $review; ?></textarea>
+                            <br><br>
+                            <b>Beoordeling:</B><br>
+                            <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "uitstekend") echo "checked"; ?> value="uitstekend">Uitstekend
+                            <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "goed") echo "checked"; ?> value="goed">Goed
+                            <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "matig") echo "checked"; ?> value="matig">Matig
+                            <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "slecht") echo "checked"; ?> value="slecht">Slecht
+                            <input type="radio" name="beoordeling" <?php if (isset($beoordeling) && $beoordeling == "nietAanbevelen") echo "checked"; ?> value="nietAanbevelen">Niet aan te bevelen
 
-                        <span class="error">* <?php echo $beoordelingErr; ?></span>
-                        <br><br>
-                        <input type="submit" name="submit" value="Stuur!">
+                            <span class="error">* <?php echo $beoordelingErr; ?></span>
+                            <br><br>
+                            <input type="submit" name="submit" value="Stuur!">
+                        </div>
                     </fieldset>
                 </form>
             </div>
             <div class="item4">view
+
                 <?php
                 echo "<h2>Uw invoer:</h2>";
-                echo $naam;
+                echo "<i>$naam</i>";
                 echo "<br>";
-                echo $achternaam;
+                echo "<i>$achternaam</i>";
                 echo "<br>";
                 echo $email;
                 echo "<br>";
@@ -189,6 +203,37 @@ $conn = connectionDB();
                 echo "<br>";
                 echo $beoordeling;
                 ?>
+            </div>
+
+            <div class="item4">
+                <div>
+                    <?php
+                    $conn = connectionDB();
+                    if ($conn->connect_error)
+                        die($conn->connect_error);
+
+                    $query = "SELECT * FROM reviews";
+                    $result = $conn->query($query);
+                    if (!$result)
+                        die("Database access failed: " . $conn->error);
+
+                    $rows = $result->num_rows;
+
+                    for ($j = 0; $j < $rows; ++$j) {
+                        $result->data_seek($j);
+                        $row = $result->fetch_array(MYSQLI_NUM);
+                        echo <<<_END
+      <br><br>Naam :<span id="pre"> $row[1] $row[2]</span><br>
+       Beoordeling :<span id="pre"> $row[5]</span>               
+<pre></pre>
+Review:<div id="pre">$row[4]</div><br><br><hr> 
+_END;
+                    }
+
+                    $result->close();
+                    $conn->close();
+                    ?>
+                </div>
             </div>
             <div class="item5">Right</div>
             <div class="item6">Footer</div>
